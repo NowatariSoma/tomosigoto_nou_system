885a04653aeaeef475778d9dc764584a3fd2fda5
NowatariSoma <nowasoma17nowa@gmail.com>
2025-08-22 03:03:10 +0900

    [FEAT] サインインとかをやる

--- backend/app/core/error_messages.py
+++ backend/app/core/error_messages.py
@@ -40,7 +40,7 @@ class ErrorMessage:
         text = "ユーザーが見つかりません"
     
     class USER_ALREADY_EXISTS(BaseMessage):
-        status_code = status.HTTP_400_BAD_REQUEST
+        status_code = status.HTTP_409_CONFLICT
         text = "このメールアドレスは既に使用されています"
     
     class INACTIVE_USER(BaseMessage):
