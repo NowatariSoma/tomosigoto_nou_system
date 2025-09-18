4d7f20b8337b3ea80481bc518dbc028de678adaf
MiyuKUMAGAI <terrybear1863@gmail.com>
2025-09-18 16:06:22 +0900

    [FEAT] 演目の一覧を表示するページを追加した

--- frontend/performance-list/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import FloraDisplay from './components/FloraDisplay';
-import { mockFloraData } from './data/mockFlora';
-
-export default function Home() {
-  return <FloraDisplay sections={mockFloraData.sections} />;
-}
