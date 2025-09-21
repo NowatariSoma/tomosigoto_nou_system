22c83ece84559a70b5974f6afe4167e31717271d
Pens-1 <d0193480501@gmail.com>
2025-09-21 13:11:28 +0000

    [FIX]　ルーティングにミスがあったため修正

--- frontend/features/register/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as RegisterPage } from './RegisterPage';
-export { default as RoomSelection } from './RoomSelection';
-export { default as DateTimeSelection } from './DateTimeSelection';
-export { default as DateSelection } from './DateSelection';
-export { default as TimeSelection } from './TimeSelection';
