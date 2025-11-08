580934eea25a2f02fb4dc7b44ed57064cdc75fd5
NowatariSoma <nowasoma17nowa@gmail.com>
2025-11-08 22:28:47 +0900

    [FEAT] コンフリクト解消

--- frontend/features/practice-slots/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { PracticeSlotsPage } from './PracticeSlotsPage';
-export { Information } from './Information';
-export { ScheduleTable } from './ScheduleTable';
-export { DateButton } from './DateButton';
-export { InstructorDisplay } from './InstructorDisplay';
-export { AttendanceSummary } from './AttendanceSummary';
